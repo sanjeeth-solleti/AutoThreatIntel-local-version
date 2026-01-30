5b4f12b2c3ae251cf8041dfb9dc60e0fbd83984f
solleti vasava sanjeeth <vasavasanjeeth@gmail.com>
2026-01-30 11:03:20 +0530

    Create app.js

---
+++ frontend/src/app.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "40px" }}>
+      <h1>ThreatLens Frontend is Live 🚀</h1>
+      <p>React + Vercel deployment successful.</p>
+    </div>
+  );
+}
+
+export default App;
